86f20f0d6c787077b8c02de284b26491c7f17f5a
Fernando Nina <Fernandonina611@gmail.com>
2026-02-23 20:19:13 -0400

    feat: :tada: configuracion inicial del proyetco

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/status')
+def index():
+    data = {
+        "status": "online",
+        "mensaje": "La API del Taller está funcionando",
+        "version": 1.0,
+        "servidor": "Flask/Werkzeug"
+    }
+    return jsonify(data)
+
+@app.route('/html')
+def html():
+    return render_template('index.html')
+
+@app.route('/saludo')
+def saludo():
+    return 'Hola bienvenido a Taller Apps'
+
+@app.route('/usuario/<nombre>')
+def usuario(nombre):
+    return f'Hola {nombre} bienvenido a Taller Apps'
+
+if __name__ == '__main__':
+    app.run(debug=True)
